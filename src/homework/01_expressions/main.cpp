9306537a063dd43e50a2cda7b268a9add49e4c2a
dmurtiono <david.murtiono@g.austincc.edu>
2020-01-29 18:48:25 -0600

    hw1 - multiply func + unit test

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -1,15 +1,18 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "expressions.h"
 
 //write namespace using statement for cout
-
+using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int product = multiply_numbers(10, 10);
+
+	cout << "Result: " << product;
 
 	return 0;
 }
