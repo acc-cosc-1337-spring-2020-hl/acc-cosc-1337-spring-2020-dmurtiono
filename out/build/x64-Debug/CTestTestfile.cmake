82b202929efacb34797349518f5ba6d703665f9e
dmurtiono <david.murtiono@g.austincc.edu>
2020-01-31 01:24:58 -0600

    sync

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Programming/C++/ACC
+# Build directory: D:/Programming/C++/ACC/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ out/build/x64-Debug/test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: D:/Programming/C++/ACC/test
+# Build directory: D:/Programming/C++/ACC/out/build/x64-Debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("classwork_test")
+subdirs("examples_test")
+subdirs("homework_test")
