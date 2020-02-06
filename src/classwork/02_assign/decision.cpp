6c734cb9496f575d7683dc05da64e0a6ea57e4c2
dmurtiono <david.murtiono@g.austincc.edu>
2020-02-05 19:00:02 -0600

    assignment 2: gross_pay function, unit tests

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,17 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+double gross_pay(double hours, double hourly_rate)
+{
+	if (hours > 40) 
+	{
+		double overtime = hours - 40;
+		hours -= overtime;
+
+		hours += overtime * 1.5;
+	}
+
+	double gross = hours * hourly_rate;
+
+	return gross;
+}
--- test/classwork_test/02_assign_test/decisions_tests.cpp
+++ test/classwork_test/02_assign_test/decisions_tests.cpp
@@ -1,8 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "decision.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Tests for gross_pay function") {
+	REQUIRE(gross_pay(10, 15) == 150);
+	REQUIRE(gross_pay(5, 20) == 100);
+	REQUIRE(gross_pay(20, 10) == 200);
+	REQUIRE(gross_pay(41, 10) == 415);
+	REQUIRE(gross_pay(50, 20) == 1100);
+	REQUIRE(gross_pay(11.8, 12.25) == 144.55);
+}
