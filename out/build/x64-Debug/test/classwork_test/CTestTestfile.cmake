5afa534f04ff43b53192244d6f2d1ef2d4565ccd
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-05 22:36:51 -0600

    not required

--- out/build/x64-Debug/test/classwork_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming/C++/ACC/test/classwork_test
-# Build directory: D:/Programming/C++/ACC/out/build/x64-Debug/test/classwork_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("01_assign_test")
